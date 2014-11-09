7b892a03c6774d816ff333cce804375864df1ff3
Michel Albert <michel@albert.lu>
2014-11-09 17:18:27 +0100

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    version='1.0',
+    name='pira',
+    packages=find_packages(),
+    install_requires=[
+        'python-musicpd'
+    ])
